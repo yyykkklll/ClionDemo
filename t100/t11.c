e14cee238bb0dc133450606038095813dc99ce33
ykl <1175265528@qq.com>
2024-02-02 15:38:49 +0800

    第2次提交

--- t100/t11.c
+++ t100/t11.c
@@ -4,7 +4,18 @@
  */
 
 #include <stdio.h>
-int main(){
-    printf("上岸");
+
+int main() {
+    int integerType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+    // sizeof 操作符用于计算变量的字节大小
+    printf("Size of int: %llu bytes\n", sizeof(integerType));
+    printf("Size of float: %llu bytes\n", sizeof(floatType));
+    printf("Size of double: %llu bytes\n", sizeof(doubleType));
+    printf("Size of char: %llu byte\n", sizeof(charType));
+
     return 0;
 }
--- t100/t12.c
+++ t100/t12.c
@@ -1,7 +1,25 @@
-/********************************************************************************
-* @author: yklsfzy
-* @email: 1175265528@qq.com
-* @date: 2024/1/31/0031 23:07:35
-* @version: 1.0
-* @description: 
-********************************************************************************/
+/**
+ * @author yklsfzy
+ * @date 2024/1/31/0031 22:58:12
+ */
+
+#include <stdio.h>
+void swap_01(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void swap_02(int *a, int *b){
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+int main(){
+    int firsrnumber,secondnumber;
+    printf("Enter the two numbers: ");
+    scanf("%d%d", &firsrnumber, &secondnumber);
+    printf("Before swap: %d %d\n", firsrnumber, secondnumber);
+    swap_02(&firsrnumber, &secondnumber);
+    printf("After swap: %d %d\n", firsrnumber, secondnumber);
+    return 0;
+}

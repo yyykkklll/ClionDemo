2b88f62528117358a8f42f165395d4e1c7bc9aa9
ykl <1175265528@qq.com>
2024-02-20 16:32:40 +0800

    复试练习

--- chapter_01/test_02.c
+++ chapter_01/test_02.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("hello world");
+    char s[] = "Hello, world!";
+    char r[100];
+    memcpy(r, s, strlen(s));
+    printf("%s\n", r);
     return 0;
 }
